fbaf85ac48f33c017b2a2e9e131033275bc59636
Sofian Hammou <sofianhammou30@gmail.com>
2024-05-27 12:24:18 -0700

    Changed background, edited button color, changec container color, added text wrapping, added spaces, and removed duplicate attractions.

--- src/components/TouristLocation.js
+++ src/components/TouristLocation.js
@@ -1,16 +1,33 @@
 import React from 'react'; // Importing React
 import PropTypes from 'prop-types'; // Importing PropTypes for type checking
 
+// Function to remove duplicate tourist locations
+const removeDuplicates = (locations) => {
+    const uniqueLocations = [];
+    const seenNames = new Set();
+
+    locations.forEach(location => {
+        if (!seenNames.has(location.properties.name)) {
+            uniqueLocations.push(location);
+            seenNames.add(location.properties.name);
+        }
+    });
+
+    return uniqueLocations;
+};
+
 // Functional component to display tourist locations
 function TouristLocations({ locations }) { 
+    const uniqueLocations = removeDuplicates(locations);
+
     return (
         <div>
             <h2>Tourist Locations</h2>
-            <ul>
-                {locations.map((location, index) => (
-                    <li key={index}>
-                        <h3>{location.properties.name}</h3>
-                        <p>{location.properties.kinds}</p>
+            <ul style={{ padding: 0, listStyle: 'none' }}>
+                {uniqueLocations.map((location, index) => (
+                    <li key={index} style={{ marginBottom: '10px', overflowWrap: 'break-word', wordWrap: 'break-word' }}>
+                        <h3 style={{ margin: 0 }}>{location.properties.name}</h3>
+                        <p style={{ margin: 0 }}>{location.properties.kinds.split(',').join(', ')}</p>
                     </li>
                 ))}
             </ul>
